ed28f30e73de5355bfba91d68ec078e9fe10bc2c
Ewerson Castelo <ewerson.castelo@gmail.com>
2020-09-22 23:01:14 -0400

    redesign home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
-import 'package:NotePad/Home.dart';
+import 'package:NotePad/screens/todo_list_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         fontFamily: 'SF Pro Text Regular',
+//       ),
+//       home: Home(),
+//     );
+//   }
+// }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Todo List',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'SF Pro Text Regular',
+        primarySwatch: Colors.red,
       ),
-      home: Home(),
+      home: TodoListScreen(),
     );
   }
 }
